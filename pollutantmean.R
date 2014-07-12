cafece5ac3ac6c178d018a054beafc26c2a7b670
Andy Wilson <atwilso@sandia.gov>
2014-07-11 22:19:12 -0600

    Code for programming assignment 1
    In which I learn to use rbindlist and sapply.
    
    R turns out to have several different 'apply' functions.  I will
    probably learn the differences between all of them.

---
+++ pollutantmean.R
@@ -0,0 +1,30 @@
+## R Programming
+## Coursera Data Science Specialization
+## Programming Assignment 2: I Can't Breathe!
+
+library(data.table)
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    # Compute the mean for one pollutant over some set of sensors.
+    #
+    # Args:
+    #   directory: Character vector: directory containing the data files
+    #   pollutant: Character vector: which pollutant to compute (currently "sulfate" or "nitrate")
+    #   id: Integer vector:  which sensors to use to compute the mean
+    #
+    # Returns:
+    #   Sample mean of the desired pollutant at the specified sensors
+
+    loadSingleFile <- function(fileId) {
+        filename <- sprintf("%s/%03d.csv", directory, as.integer(fileId))
+        message(sprintf("Reading data table from %s", filename))
+        read.csv(filename, header=TRUE)
+    }
+
+    framesFromFiles <- lapply(id, loadSingleFile)
+    allData <- rbindlist(framesFromFiles)
+
+    myColumn <- allData[[pollutant]]
+    cleanedColumn <- myColumn[!is.na(myColumn)]
+    mean(cleanedColumn)
+}
